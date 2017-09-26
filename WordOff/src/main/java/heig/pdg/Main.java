94870c0d6f9977b8ff85d4d3f5849fce537d8030
Bertral <antoine.friant@gmail.com>
2017-09-26 11:25:46 +0200

    project init

---
+++ WordOff/src/main/java/heig/pdg/Main.java
@@ -0,0 +1,13 @@
+package heig.pdg;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

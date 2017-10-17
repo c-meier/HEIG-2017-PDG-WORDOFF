06db42babd11f7c01056fc29bc7e4c75156615e2
Michela Zucca <michela.zucca@heig-vd.ch>
2017-10-17 19:09:39 +0200

    Client : modif Bag

--- WordOff/Client/src/main/java/ch/heigvd/wordoff/logic/Bag.java
+++ WordOff/Client/src/main/java/ch/heigvd/wordoff/logic/Bag.java
@@ -3,20 +3,26 @@ package ch.heigvd.wordoff.logic;
 import java.util.ArrayList;
 
 public class Bag {
-    private final int numberTilesMax = 115;
     private int numberTiles;
 
-    public Bag(){
-        this.numberTiles = numberTilesMax;
+    public Bag(int numberTiles){
+        this.numberTiles = numberTiles;
     }
 
     public ArrayList<Tile> getTiles(int number){
         ArrayList<Tile> tiles = new ArrayList<>();
-        if(number > numberTiles)
+        if(number > numberTiles) {
             number = numberTiles;
-
+        }
         // requête au serveur
         // construction de tiles
+
+        // Comptabilise le retrait si requête en ordre
+        numberTiles -= tiles.size();
         return tiles;
+    }
+
+    public int getNumberTiles(){
+        return numberTiles;
     }
 }

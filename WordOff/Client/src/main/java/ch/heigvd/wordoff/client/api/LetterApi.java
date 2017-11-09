ad9dfb1287641eb97ead0fa2d99a045162643565
Gabriel luthier <gabriel.luthier@heig-vd.ch>
2017-11-09 11:30:33 +0100

    remove LangSetDto usage

--- WordOff/Client/src/main/java/ch/heigvd/wordoff/client/api/LetterApi.java
+++ WordOff/Client/src/main/java/ch/heigvd/wordoff/client/api/LetterApi.java
@@ -1,7 +1,6 @@
 package ch.heigvd.wordoff.client.api;
 
 import ch.heigvd.wordoff.client.Exception.BadRequestException;
-import ch.heigvd.wordoff.common.Dto.Tiles.LangSetDto;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.xml.ws.http.HTTPException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static ch.heigvd.wordoff.common.Constants.SERVER_URI;
@@ -18,7 +18,7 @@ public class LetterApi {
 
     private final static RestTemplate restTemplate = new RestTemplate();
 
-    public static LangSetDto retrieveLetters(String lang) {
+    public static List<Character> retrieveLetters(String lang) {
         final String uri = SERVER_URI + "/letters/{lang}";
 
         Map<String, String> params = new HashMap<>();
@@ -35,7 +35,7 @@ public class LetterApi {
 
         switch (responseEntity.getStatusCode()) {
             case OK: // 200
-                return (LangSetDto)responseEntity.getBody();
+                return (List<Character>)responseEntity.getBody();
             case BAD_REQUEST: // 400
                 throw new BadRequestException();
             default:
--- WordOff/Common/src/main/java/ch/heigvd/wordoff/common/Dto/Tiles/LangSetDto.java
+++
@@ -1,4 +0,0 @@
-package ch.heigvd.wordoff.common.Dto.Tiles;
-
-public class LangSetDto {
-}

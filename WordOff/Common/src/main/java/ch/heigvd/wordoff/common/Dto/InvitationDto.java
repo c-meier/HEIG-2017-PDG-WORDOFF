81826394a1a40ed699253a4c82cbd44b666c3973
Christopher Meier <christopher.meier@heig-vd.ch>
2017-11-21 11:47:15 +0100

    InvitationDto is an endpoint (allow accepting or denying an invitation)

--- WordOff/Common/src/main/java/ch/heigvd/wordoff/common/Dto/InvitationDto.java
+++ WordOff/Common/src/main/java/ch/heigvd/wordoff/common/Dto/InvitationDto.java
@@ -1,4 +1,41 @@
 package ch.heigvd.wordoff.common.Dto;
 
-public class InvitationDto {
+import ch.heigvd.wordoff.common.Dto.Endpoint.IResource;
+
+public class InvitationDto implements IResource<InvitationDto> {
+    private Long invitationId;
+
+    private InvitationStatus status;
+    /**
+     * Endpoint to update (PUT) the status of an invitation.
+     */
+    private String endpoint;
+
+    public Long getInvitationId() {
+        return invitationId;
+    }
+
+    public void setInvitationId(Long invitationId) {
+        this.invitationId = invitationId;
+        if(invitationId != null) {
+            setEndpoint("/me/invitations/" + invitationId);
+        }
+    }
+
+    public InvitationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(InvitationStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
 }

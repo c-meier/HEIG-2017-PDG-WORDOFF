f03bdf9118dc460278db858761726ae2bfa5cd79
Christopher Meier <christopher.meier@heig-vd.ch>
2017-11-21 10:19:41 +0100

    Relation entity (friend + blacklist)

--- WordOff/Server/src/test/java/ch/heigvd/wordoff/server/UserRepositoryTest.java
+++ WordOff/Server/src/test/java/ch/heigvd/wordoff/server/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package ch.heigvd.wordoff.server;
 
+import ch.heigvd.wordoff.common.Dto.User.RelationStatus;
 import ch.heigvd.wordoff.server.Model.Credentials;
 import ch.heigvd.wordoff.server.Model.User;
 import ch.heigvd.wordoff.server.Repository.UserRepository;
@@ -36,5 +37,31 @@ public class UserRepositoryTest {
 
         assertThat(savedUser).isNotNull();
         assertThat(savedUser.getCredentials().getLogin()).isEqualTo(LOGIN);
+    }
+
+    @Test
+    public void testUserCanHaveRelations() throws Exception {
+        User one = new User("testOne");
+        User two = new User("testTwo");
+        User three = new User("testThree");
+
+        one = repository.save(one);
+        two = repository.save(two);
+        three = repository.save(three);
+
+        assertThat(one).isNotNull();
+        assertThat(two).isNotNull();
+        assertThat(three).isNotNull();
+
+        one.setRelation(two, RelationStatus.FRIEND);
+        one.setRelation(three, RelationStatus.BLOCKED);
+        two.setRelation(three, RelationStatus.FRIEND);
+
+        assertThat(one.getRelations().size()).isEqualTo(2);
+
+        one.setRelation(two, RelationStatus.NONE);
+
+        User savedOne = repository.findByName("testOne");
+        assertThat(savedOne.getRelations().size()).isEqualTo(1);
     }
 }

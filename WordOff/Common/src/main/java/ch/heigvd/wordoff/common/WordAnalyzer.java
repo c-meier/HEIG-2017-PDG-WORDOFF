42f8dba86f67d1b9b370cfff30978211d952d906
Bertral <antoine.friant@gmail.com>
2017-12-14 08:35:12 +0100

    Hint non testé

--- WordOff/Common/src/main/java/ch/heigvd/wordoff/common/WordAnalyzer.java
+++ WordOff/Common/src/main/java/ch/heigvd/wordoff/common/WordAnalyzer.java
@@ -19,11 +19,65 @@ import java.util.List;
  */
 public class WordAnalyzer {
 
-    /*
+    /**
+     * @param dico       Dictionnaire à utiliser
+     * @param challIn    Challenge dont les Slots sont vides
+     * @param plRackIn   PlayerRack (plein)
+     * @param beginsWith Premières lettres posées
+     * @return liste des Tiles à remettre surle Challenge
+     */
+    public static List<ITile> getHint(Dictionary dico, IChallenge challIn, IRack plRackIn, List<ITile> beginsWith) {
+        IChallenge challenge = challIn.duplicate();
+        List<Pair<Integer, List<ITile>>> bestWords = getWordsByScore(dico, challIn, plRackIn);
+        List<ITile> bestWord = bestWords.get(bestWords.size() - 1).getValue();
+        List<ITile> result = new ArrayList<>(beginsWith);
+
+        if (bestWords.isEmpty()) {
+            // S'il n'y a pas de solution, on renvoie une liste vide.
+            result.clear();
+            return result;
+        }
+
+        if (bestWord.size() < result.size()) {
+            // Si le mot entré est plus grand que la solution optimale, on vide le mot
+            result.clear();
+        }
+
+        // Vide le mot entré si une lettre ne correspond pas
+        for (int i = 0; i < result.size(); ++i) {
+            if (result.get(i).getValue() != bestWord.get(i).getValue()) {
+                result.clear();
+                break;
+            }
+        }
+
+        // Si toutes les lettres correspondaient au début du mot optimal et le mot optimal n'est pas déjà en input,
+        // on ajoute la lettre suivante
+        if (bestWord.size() != result.size()) {
+            result.add(bestWord.get(result.size()));
+        }
+
+        return result;
+    }
+
+    /**
      * Calcule les scores des mots possibles et renvoie les mots dans l'ordre croissant du score
      * qu'ils marqueraient sur ce Side. Prend en compte le SwapRack. La clé du la paire est le score.
+     * L'array renvoyé est vide si aucun mot n'est possible.
      *
-     * @return List<Pair<Integer (score), List<ITile> (mot)>>
+     * @param dico     Dictionnaire à utiliser
+     * @param challIn  Challenge dont les slots sont vides
+     * @param plRackIn PlayerRack (plein)
+     * @return List<Pair
+                                        * <
+                                                                                 *   Integer
+                       *       (
+                                                                  *       score
+                       *       )
+                                                                  *       ,
+                       *       List
+                                                                     *       <
+                       *       ITile> (mot)>>
      */
     public static List<Pair<Integer, List<ITile>>> getWordsByScore(Dictionary dico, IChallenge challIn,
                                                                    IRack plRackIn) {
@@ -49,7 +103,7 @@ public class WordAnalyzer {
                     new SwapRackDto(new ArrayList<>(challenge.getSwapRack().getTiles())));
             tempChall.getSlots().forEach(ISlot::removeTile);
             IRack playerRack = new PlayerRackDto(new ArrayList<>());
-            plRackIn.getTiles().forEach((el)->playerRack.addTile(el.duplicate()));
+            plRackIn.getTiles().forEach((el) -> playerRack.addTile(el.duplicate()));
             SwapRackDto swapRack = new SwapRackDto(new ArrayList<>());
             challenge.getSwapRack().getTiles().forEach(swapRack::addTile);
 

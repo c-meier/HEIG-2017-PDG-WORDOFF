7d0dc19c38b0afb45ff618b95bb816cd22245718
Christopher Meier <christopher.meier@heig-vd.ch>
2018-01-01 20:03:18 +0100

    Comments to the MeController.

--- WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Rest/Endpoint/MeController.java
+++ WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Rest/Endpoint/MeController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Controller that allow for actions pertaining to the current user.
+ */
 @RestController
 @RequestMapping(value = "/me", produces = "application/json")
 public class MeController {
@@ -28,12 +31,22 @@ public class MeController {
         this.modeService = modeService;
     }
 
+    /**
+     * GET information and endpoints about the given logged player.
+     * @param player The current player.
+     * @return The information and endpoints about the current logged user.
+     */
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<MeDto> getMe(
             @RequestAttribute("player") User player) {
         return new ResponseEntity<>(DtoFactory.createMeFrom(player), HttpStatus.OK);
     }
 
+    /**
+     * GET the list of notifications pertaining to the given player.
+     * @param player The current player.
+     * @return The list of notifications.
+     */
     @RequestMapping(value = "/notifications", method = RequestMethod.GET)
     public ResponseEntity<List<NotificationDto>> getNotifications(
             @RequestAttribute("player") User player) {
@@ -43,6 +56,11 @@ public class MeController {
                 .collect(Collectors.toList()), HttpStatus.OK);
     }
 
+    /**
+     * GET the list of invitations for the given player that have not yet been accepted or denied.
+     * @param player The current player.
+     * @return The list of invitations.
+     */
     @RequestMapping(value = "/invitations", method = RequestMethod.GET)
     public ResponseEntity<List<InvitationDto>> getInvitations(
             @RequestAttribute("player") User player) {
@@ -52,6 +70,13 @@ public class MeController {
                 .collect(Collectors.toList()), HttpStatus.OK);
     }
 
+    /**
+     * Change the status of an invitation for a given player to a given mode.
+     * @param player The current player.
+     * @param modeId The id of the mode linked to the invitation.
+     * @param invitation The invitation with the new values.
+     * @return An empty response.
+     */
     @RequestMapping(value = "/invitations/{modeId}", method = RequestMethod.PUT)
     public ResponseEntity putInvitation(
             @RequestAttribute("player") User player,
@@ -63,6 +88,11 @@ public class MeController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * GET the list of recent adversaries (players against which the given player has duelled).
+     * @param player The current player.
+     * @return The list of adversaries, each adversary given in a summarized form.
+     */
     @RequestMapping(value = "/adversaries", method = RequestMethod.GET)
     public ResponseEntity<List<UserSummaryDto>> getAdversaries(
             @RequestAttribute("player") User player) {
@@ -72,6 +102,12 @@ public class MeController {
                 .collect(Collectors.toList()), HttpStatus.OK);
     }
 
+    /**
+     * GET the list of relations (FRIENDS and BLACKLISTED users) of the given player.
+     * @param player The current player.
+     * @return The list of relations, each relation given in a summarized form with data about the
+     *      type of relation.
+     */
     @RequestMapping(value = "/relations", method = RequestMethod.GET)
     public ResponseEntity<List<RelatedUserSummaryDto>> getRelations(
             @RequestAttribute("player") User player) {
@@ -82,6 +118,13 @@ public class MeController {
 
     }
 
+    /**
+     * Change the status of a relation between a given player and the target of the given relation.
+     * @param player The current player.
+     * @param targetId The id of the target of the relation.
+     * @param relation The relation with the new status.
+     * @return The updated relation.
+     */
     @RequestMapping(value = "/relations/{userId}", method = RequestMethod.PUT)
     public ResponseEntity<RelationDto> putRelation(
             @RequestAttribute("player") User player,

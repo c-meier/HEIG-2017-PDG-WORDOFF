ac74b3acb60f303726349c1e6c04baf402e734c5
Bertral <antoine.friant@gmail.com>
2018-01-07 18:22:18 +0100

    j'ai rien dit

--- WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Service/GameService.java
+++ WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Service/GameService.java
@@ -133,8 +133,7 @@ public class GameService {
             // Update the player side
             updatePlayerSide(side, side.getChallenge(), newTiles);
 
-            // switch player and reset wordanalyzer
-            game.getSideOfPlayer(player).setWordanalyser(false);
+            // switch player
             game.setCurrPlayer(game.getOtherPlayer(player));
         } else {
             throw new ErrorCodeException(Protocol.NOT_YOUR_TURN, "Not player turn to play !");
@@ -306,8 +305,7 @@ public class GameService {
         // update
         updatePlayerSide(game.getSideOfPlayer(player), game.getSideOfPlayer(player).getChallenge(), newTiles);
 
-        // switch player and reset wordanalyzer
-        game.getSideOfPlayer(player).setWordanalyser(false);
+        // switch player
         game.setCurrPlayer(game.getOtherPlayer(player));
 
         gameRepository.save(game);
@@ -383,8 +381,7 @@ public class GameService {
         // update
         updatePlayerSide(game.getSideOfPlayer(player), game.getSideOfPlayer(player).getChallenge(), new ArrayList<>());
 
-        // switch player and reset wordanalyzer
-        game.getSideOfPlayer(player).setWordanalyser(false);
+        // switch player
         game.setCurrPlayer(game.getOtherPlayer(player));
 
         // Si l'adversaire a aussi passé son tour (sa dernière réponse est vide) et qu'il n'y a plus de pièces à ajouter

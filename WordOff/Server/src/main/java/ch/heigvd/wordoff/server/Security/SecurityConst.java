ff9c65a529489e291b4a5c83b1f983b697bfecd9
Bertral <antoine.friant@gmail.com>
2018-01-15 15:25:27 +0100

    prolongation de la durée de vie du token (10h)

--- WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Security/SecurityConst.java
+++ WordOff/Server/src/main/java/ch/heigvd/wordoff/server/Security/SecurityConst.java
@@ -4,5 +4,5 @@ public interface SecurityConst {
     String AUTH_HEADER = "Authorization";
     String TOKEN_PREFIX = "Bearer ";
     String TOKEN_SECRET = "fpjo+i3nie0*v98ij3&2f)(a";
-    long TOKEN_LENGTH_LIFE = 3_600_000; // 1 hour
+    long TOKEN_LENGTH_LIFE = 36_000_000; // 1 hour
 }
